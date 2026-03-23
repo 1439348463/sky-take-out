85b5f7c15b0579fd434b686c13e26bf43df829cf
1439348463 <522025230027@smail.nju.edu.cn>
2026-03-23 17:52:01 +0800

    hmdp第一次提交

---
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.hmdp.service.impl;
+
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.ShopType;
+import com.hmdp.mapper.ShopTypeMapper;
+import com.hmdp.service.IShopTypeService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOPTYPE_KEY;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Service
+public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryList() {
+        String key = CACHE_SHOPTYPE_KEY;
+        String typeJson = stringRedisTemplate.opsForValue().get(CACHE_SHOPTYPE_KEY);
+        if(StrUtil.isNotBlank(typeJson)){
+            List<ShopType> shopTypeList = JSONUtil.toList(typeJson, ShopType.class);
+            return Result.ok(shopTypeList);
+        }
+        List<ShopType> shopTypeList = query().orderByAsc("sort").list();
+        stringRedisTemplate.opsForValue().set(CACHE_SHOPTYPE_KEY, JSONUtil.toJsonStr(shopTypeList), 30, TimeUnit.MINUTES);
+        return Result.ok(shopTypeList);
+    }
+}
---
+++ src/main/java/com/hmdp/utils/CacheClient.java
@@ -0,0 +1,113 @@
+package com.hmdp.utils;
+
+import cn.hutool.core.util.BooleanUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.entity.RedisData;
+import com.hmdp.entity.Shop;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
+
+import static com.hmdp.utils.RedisConstants.*;
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+
+@Slf4j
+@Component
+public class CacheClient {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public CacheClient(StringRedisTemplate stringRedisTemplate) {
+        this.stringRedisTemplate = stringRedisTemplate;
+    }
+
+    public void set(String key, Object value, Long time, TimeUnit unit){
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(value), time, unit);
+    }
+
+    public void setWithLogicalExpire(String key, Object value, Long time, TimeUnit unit){
+        RedisData redisData = new RedisData();
+        redisData.setData(value);
+        redisData.setExpireTime(LocalDateTime.now().plusSeconds(unit.toSeconds(time)));
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(redisData));
+    }
+
+    //缓存穿透
+    public <R, ID> R queryWithPassThrough(String keyPrefix, ID id, Class<R> type, Function<ID, R> dbFallBcak, Long time, TimeUnit unit){
+        String key = keyPrefix + id;
+        String json = stringRedisTemplate.opsForValue().get(key);
+        if (StrUtil.isNotBlank(json)) {
+            return JSONUtil.toBean(json, type);
+        }
+        //判断命中的是否是空值
+        if(json != null){
+            return null;
+        }
+        R r = dbFallBcak.apply(id);
+        if(r == null){
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
+            return null;
+        }
+
+        //stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(r), CACHE_SHOP_TTL, TimeUnit.MINUTES);
+        this.set(key, r, time, unit);
+
+        return r;
+    }
+
+    private static final ExecutorService CACHE_REBUILD_EXECUTOR = Executors.newFixedThreadPool(10);
+
+
+    public <R, ID> R queryWithLogicalExpire(String keyPrefix, ID id, Class<R> type, Function<ID, R> dbFallback, Long time, TimeUnit unit){
+        String key = keyPrefix + id;
+        String json = stringRedisTemplate.opsForValue().get(key);
+        if (StrUtil.isBlank(json)) {
+            return null;
+        }
+        RedisData redisData = JSONUtil.toBean(json, RedisData.class);
+        R r = JSONUtil.toBean((JSONObject) redisData.getData(), type);
+        LocalDateTime expireTime = redisData.getExpireTime();
+
+        if(expireTime.isAfter(LocalDateTime.now())){
+            return r;
+        }
+
+        String lockKey = LOCK_SHOP_KEY + id;
+        boolean isLock = tryLock(lockKey);
+
+        if(isLock){
+            CACHE_REBUILD_EXECUTOR.submit(() -> {
+                try {
+                    //重建缓存
+                    R r1 = dbFallback.apply(id);
+                    //写入redis
+                    this.setWithLogicalExpire(key, r1, time, unit);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                } finally {
+                    unlock(lockKey);
+                }
+
+            });
+        }
+
+        return r;
+    }
+
+    private boolean tryLock(String key){
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(key, "1", 10, TimeUnit.SECONDS);
+        return BooleanUtil.isTrue(flag);
+    }
+
+    private void unlock(String key){
+        stringRedisTemplate.delete(key);
+    }
+}
